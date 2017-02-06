f26ef5c2c7954fc79d5a1c2f3464c0e1fc73e99e
jnopnop <jnopnop@users.noreply.github.com>
2017-02-07 00:06:46 +0200

    async utils javadoc

--- src/main/java/org/nop/utils/AsyncUtils.java
+++ src/main/java/org/nop/utils/AsyncUtils.java
@@ -11,12 +11,29 @@ import static org.nop.utils.functional.ternary.When.when;
 
 public class AsyncUtils {
 
+    /**
+     * Convert ListenableFuture to native CompletableFuture, that is mapped onto corresponding success and error
+     * callbacks.
+     * @param listenableFuture target listenableFuture
+     * @param <T> result type
+     * @return completable future
+     */
     public static <T> CompletableFuture<T> toCompletableFuture(final ListenableFuture<T> listenableFuture) {
         final CompletableFuture<T> completableFuture = new CompletableFuture<>();
         listenableFuture.addCallback(completableFuture::complete, completableFuture::completeExceptionally);
         return completableFuture;
     }
 
+    /**
+     * Obtains result from completable future which will be processed when completable future is finished.
+     * @param completableFuture asynchronous computation
+     * @param resultSupplier supplies the result that will be returned i.e. <em>target result</em>
+     * @param successConsumer bi-function that takes <em>target result</em> and the result of async computation
+     * @param errorConsumer bi-function that takes <em>target result</em> and an in instance of Throwable in case of error
+     * @param <T> <em>target result</em> type
+     * @param <U> async computation result type
+     * @return <em>target result</em>
+     */
     public static <T, U> T mapToAsyncResult(final CompletableFuture<U> completableFuture, final Supplier<? extends T> resultSupplier,
                                        final BiConsumer<? super T, ? super U> successConsumer,
                                        final BiConsumer<? super T, ? super Throwable> errorConsumer) {
